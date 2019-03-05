7e86cdfff8aaa88b652a483526eff06281e10552
Benjamin Martinez <bmartinez@sada.fr>
2019-03-05 13:28:44 +0100

    new function tests

--- src/grocery.kt
+++ src/grocery.kt
@@ -11,6 +11,10 @@ fun main(args: Array<String>) {
         "Vegetable",
         "1b",
         3.0,2),
+        Grocery("Mushrooms",
+            "Vegetable",
+            "1b",
+            1.5, 1),
         Grocery("Bagels",
             "Bakery",
             "Pack",
@@ -39,9 +43,59 @@ fun main(args: Array<String>) {
 
     println(fold)
 
+    groceries.groupBy { it.category }.forEach {
+      it.value.forEach { grocery: Grocery -> println(grocery) }
+    }
+
+
+    val ints = listOf(1,2,4)
+    val sumofInts = ints.fold(0) {a, b -> a+b}
+
+    val productOfInts = ints.fold(1)  {product, b -> product*b}
+
+    val names= groceries.fold("") {sentence, grocerie -> sentence+ "${grocerie.name} "}
+
+    val changeFrom50 = groceries.fold(50.0) {moneyLeft, g -> moneyLeft - (g.unitPrice * g.quantity)}
+
+
+    val totalPrice = groceries.filter { it.category == "Vegetable" }.map { it.quantity * it.unitPrice }
+        .reduce { totalP, currentP -> totalP + currentP }
+
+    val totalPrice2 = groceries.filter { it.category == "Vegetable" }.sumByDouble { it.quantity * it.unitPrice }
+
+
+    val listOfCheaps : List<String> = groceries.filter { it.unitPrice * it.quantity < 5.0 }.map { it.name }
+
+    groceries.groupBy { it.category }.forEach {
+        val total =
+            it.value.fold(0.0) { total, grocery -> (total + (grocery.quantity * grocery.unitPrice)) }
+
+        println("First ${it.key} : ${total}")
+
+    }
+
+
+    groceries.groupBy { it.category }.forEach {
+        println(" ${it.key} : ${it.value.sumByDouble { it.unitPrice * it.quantity }}")
+    }
+
+
+
+
+    groceries.filterNot { it.unit =="Bottle" }.groupBy { it.unit }.forEach {it.value.forEach { println(it.name) }}
 
 
 }
+
+
+public inline fun <T> Iterable<T>.sumByDouble(selector: (T) -> Double): Double {
+    var sum: Double = 0.0
+    for (element in this) {
+        sum += selector(element)
+    }
+    return sum
+}
+
 
 fun search(list : List<Grocery>, criteria : (g: Grocery) -> Boolean){
     for (l in list){
